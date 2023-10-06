a7cc5f866c7b2f7ee468b89ffbf87af300cd5f4b
sandyjgithub <147196650+sandyjgithub@users.noreply.github.com>
2023-10-06 15:10:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My  Parents new Healthy Diner')
+
+streamlit.heading('Breakfast Menu')
+
+streamlit.text('Breakfast Menu')
+streamlit.text('Breakfast Menu')
 streamlit.text('Breakfast Menu')
