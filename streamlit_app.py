d4929c7f4fa79637e967d9e6a4fbbac951e31b01
sandyjgithub <147196650+sandyjgithub@users.noreply.github.com>
2023-10-06 15:12:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My  Parents new Healthy Diner')
 
-streamlit.heading('Breakfast Menu')
+streamlit.title('Breakfast Menu')
 
-streamlit.text('Breakfast Menu')
-streamlit.text('Breakfast Menu')
-streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal ')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
